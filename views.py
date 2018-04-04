1487babb48379dc48c7ff4b008e6d10a35800a36
QuadX <quadx.vlad@gmail.com>
2018-04-04 20:39:39 +0300

    Added verification page

--- views.py
+++ views.py
@@ -1,5 +1,7 @@
-from flask import render_template, Blueprint
+import requests
+from flask import render_template, Blueprint, request, jsonify
 
+from settings import FACE_RECOGNITION_SERVER
 
 bp = Blueprint('views', __name__)
 
@@ -7,3 +9,17 @@ bp = Blueprint('views', __name__)
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+
+@bp.route('/verification', methods=['GET', 'POST'])
+def verification():
+    if request.method == 'GET':
+        return render_template('verification.html')
+    elif request.method == 'POST':
+        image = request.files['image']
+        return jsonify(requests.post(
+            f'{FACE_RECOGNITION_SERVER}/find-faces',
+            files={
+                'image': image.stream
+            }
+        ).json())
