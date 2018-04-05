eb7319a5574efc81beb137bd642c70225cf570a1
QuadX <quadx.vlad@gmail.com>
2018-04-05 18:02:41 +0300

    Added uploading image by URL on verification page

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 import requests
 from flask import render_template, Blueprint, request, jsonify
+from requests import RequestException
 
 from settings import FACE_RECOGNITION_SERVER
 
@@ -14,6 +15,19 @@ def index():
 @bp.route('/verification', methods=['GET', 'POST'])
 def verification():
     if request.method == 'GET':
+        image_url = request.args.get('image_url')
+        if image_url:
+            try:
+                img_data = requests.get(image_url).content
+            except RequestException:
+                return jsonify({'error': 'invalid image URL'})
+
+            return jsonify(requests.post(
+                f'{FACE_RECOGNITION_SERVER}/find-faces',
+                files={
+                    'image': img_data
+                }
+            ).json())
         return render_template('verification.html')
     elif request.method == 'POST':
         image = request.files['image']
