efce4270e2389411865fdb402f9d401c425e7bab
QuadX <quadx.vlad@gmail.com>
2018-05-28 19:20:26 +0300

    Added AddFaceForm

--- forms.py
+++ forms.py
@@ -1,13 +1,17 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileRequired
-from wtforms import StringField, TextAreaField, FileField
+from wtforms import StringField, TextAreaField, HiddenField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Email
 
 
 class AddPersonForm(FlaskForm):
-    image = FileField(validators=[FileRequired()])
+    image = HiddenField()
     email = EmailField('Электронная почта', [DataRequired(), Email()])
     first_name = StringField('Имя', [DataRequired()])
     last_name = StringField('Фамилия', [DataRequired()])
     addition_info = TextAreaField('Дополнительная информация')
+
+
+class AddFaceForm(FlaskForm):
+    image = HiddenField()
+    email = EmailField('Электронная почта', [DataRequired(), Email()])
